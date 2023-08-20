a9112014634f10599cad3176e57a8cd06893d20c
voldemort <voldemort@voldemort.myguest.virtualbox.org>
2023-08-21 00:09:03 +0300

    complete assignment one

---
+++ finder-app/finder.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+if [ $# -lt 2 ]
+    then
+        echo "error"
+        exit 1
+
+fi 
+
+filesdir=$1
+searchstr=$2
+
+if [ ! -d "$filesdir" ]
+    then 
+        echo "error"
+        exit 1
+fi
+
+numFiles=$(find "$filesdir" -type f | wc -l)
+numOfLines=$(grep -r "$searchstr" "$filesdir" | wc -l)
+
+echo "The number of files are $numFiles and the number of matching lines are $numOfLines"
---
+++ finder-app/writer.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+
+if [ $# -lt 2 ]
+then
+	echo "error"	  
+	exit 1
+
+fi
+
+
+writefile=$1
+writestr=$2
+
+mkdir -p "$(dirname "$writefile")"
+echo "$writestr" > "$writefile"
+
+if [ $? -ne 0 ]
+then 
+	echo "error"
+	exit 1
+fi
+
+echo "success"
+
+
